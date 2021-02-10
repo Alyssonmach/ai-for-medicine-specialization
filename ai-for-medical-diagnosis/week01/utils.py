5113edaa3dc7ed2600c9a2dc44af4d1c1a960016
Alyssonmach <alyssonmachado388@gmail.com>
2021-02-09 21:15:43 -0300

    Update utils.py

--- ai-for-medical-diagnosis/week01/utils.py
+++ ai-for-medical-diagnosis/week01/utils.py
@@ -14,21 +14,21 @@ def organize_csv(csv_file):
     dataframe = dataframe.drop(columns = ['Follow-up #', 'Patient Age', 'Patient Gender', 'View Position',
                                           'OriginalImage[Width', 'Height]', 'OriginalImagePixelSpacing[x', 'y]']) 
     dataframe = dataframe[(dataframe.finding_labels == 'No Finding') |
-                      (dataframe.finding_labels == 'Atelectasis') |
-                      (dataframe.finding_labels == 'Cardiomegaly') |
-                      (dataframe.finding_labels == 'Consolidation') |
-                      (dataframe.finding_labels == 'Edema') |
-                      (dataframe.finding_labels == 'Effusion') |
-                      (dataframe.finding_labels == 'Emphysema') |
-                      (dataframe.finding_labels == 'Fibrosis') |
-                      (dataframe.finding_labels == 'Hernia') |
-                      (dataframe.finding_labels == 'Infiltration') |
-                      (dataframe.finding_labels == 'Mass') |
-                      (dataframe.finding_labels == 'Nodule') |
-                      (dataframe.finding_labels == 'Pleural_Thickening') |
-                      (dataframe.finding_labels == 'Pneumonia') |
-                      (dataframe.finding_labels == 'Pneumothorax')]
+                          (dataframe.finding_labels == 'Atelectasis') |
+                          (dataframe.finding_labels == 'Cardiomegaly') |
+                          (dataframe.finding_labels == 'Consolidation') |
+                          (dataframe.finding_labels == 'Edema') |
+                          (dataframe.finding_labels == 'Effusion') |
+                          (dataframe.finding_labels == 'Emphysema') |
+                          (dataframe.finding_labels == 'Fibrosis') |
+                          (dataframe.finding_labels == 'Hernia') |
+                          (dataframe.finding_labels == 'Infiltration') |
+                          (dataframe.finding_labels == 'Mass') |
+                          (dataframe.finding_labels == 'Nodule') |
+                          (dataframe.finding_labels == 'Pleural_Thickening') |
+                          (dataframe.finding_labels == 'Pneumonia') |
+                          (dataframe.finding_labels == 'Pneumothorax')]
     dataframe = pd.get_dummies(data = dataframe, prefix = '', columns = ['finding_labels'])
     
-    return None
+    return dataframe
     
